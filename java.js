0d4506fc67fd5050d087e6672c860f3deda35a53
unknown <nolan@thewellsfamily.us>
2023-02-16 20:47:56 -0800

    added background

---
+++ java.js
@@ -0,0 +1,63 @@
+function newImage (x,y,z){
+    let newImage = document.createElement('img')
+    newImage.src = x
+    newImage.style.position = 'fixed'
+    newImage.style.left= y
+    newImage.style.bottom= z
+    document.body.append(newImage)
+}
+//let Grass = document.createElement('img')
+//Grass.src = 'assets/grass.png'
+//Grass.style.position= 'fixed'
+//Grass.style.left = '0px'
+//Grass.style.bottom = '100px'
+//document.body.append(Grass)
+
+
+///////////// grass////////////////////
+newImage('assets/grass.png',"0px","0px")
+newImage('assets/grass.png',"100px","0px")
+newImage('assets/grass.png',"200px","0px")
+newImage('assets/grass.png',"300px","0px")
+newImage('assets/grass.png',"400px","0px")
+newImage('assets/grass.png',"500px","0px")
+newImage('assets/grass.png',"600px","0px")
+newImage('assets/grass.png',"700px","0px")
+newImage('assets/grass.png',"800px","0px")
+newImage('assets/grass.png',"900px","0px")
+newImage('assets/grass.png',"0px","470px")
+newImage('assets/grass.png',"100px","470px")
+newImage('assets/grass.png',"200px","470px")
+newImage('assets/grass.png',"300px","470px")
+newImage('assets/grass.png',"400px","470px")
+newImage('assets/grass.png',"500px","470px")
+newImage('assets/grass.png',"600px","470px")
+newImage('assets/grass.png',"700px","470px")
+newImage('assets/grass.png',"800px","470px")
+newImage('assets/grass.png',"900px","470px")
+
+
+////////////water////////////////////
+//newImage('assets/water.png')
+//newImage('assets/watertwo.png')
+//newImage('assets/water3.jpg')
+//newImage('assets/water4.png')
+
+
+
+
+//////////////road//////////////////
+newImage('assets/road1.png',"0px","285px")
+newImage('assets/road1.png',"200px","285px")
+newImage('assets/road1.png',"400px","285px")
+newImage('assets/road1.png',"600px","285px")
+newImage('assets/road1.png',"738px","285px")
+newImage('assets/road1.png',"0px","100px")
+newImage('assets/road1.png',"200px","100px")
+newImage('assets/road1.png',"400px","100px")
+newImage('assets/road1.png',"600px","100px")
+newImage('assets/road1.png',"738px","100px")
+
+
+
+
