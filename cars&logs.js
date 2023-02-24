56f37294c98f87d7a5991dfdec9ec8e385db0edf
unknown <nolan@thewellsfamily.us>
2023-02-23 17:24:29 -0800

    moving logs and cars

--- cars&logs.js
+++ cars&logs.js
@@ -9,15 +9,16 @@ async function createMovingObject(f, x, y, dx, dy) {
         await sleep(1000 + Math.random()*3000)
     }
 }
-createMovingObject(Logpicture, -300, 510, 1.5, 0)
+createMovingObject(Logpicture, -300, 510, 1.2, 0)
 createMovingObject(Logpicture, 1400, 600, -.8, 0)
-createMovingObject(Logpicture, -300, 710, .7, 0)
-createMovingObject(Logpicture, 1400, 800, -1.8, 0)
-createMovingObject(Carpicture, 1400, 350, -1, 0 )
-createMovingObject(Carpicture2, -300, -30, 1, 0 )
-
-
-
+createMovingObject(Logpicture, -300, 710, .8, 0)
+createMovingObject(Logpicture, 1400, 800, -1.5, 0)
+createMovingObject(Carpicture, 1400, 350, -1.2, 0 )
+createMovingObject(Carpicture2, -300, -30, .6, 0 )
+createMovingObject(Carpicture, 1400, 220, -1.3, 0 )
+createMovingObject(Carpicture2, -300, -150, .8, 0 )
+createMovingObject(Carpicture, 1400, 100, -1.4, 0 )
+createMovingObject(Carpicture2, -300, -280, 1, 0 )
 
 function sleep (time){
     return new Promise(resolve => {
